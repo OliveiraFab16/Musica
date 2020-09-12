80a5c83bc17d52ba98e6bfc6d2ccaa8e989e7157
oliveirafab16 <fabriciocarvalho932@gmail.com>
2020-09-12 01:01:30 -0300

    letra da musica

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nome da musica:Primeiros Erros");
+            Console.WriteLine("Composição:Kiko Zambianchi.");
+            Console.WriteLine("Cantor:Dinho Ouro Preto");
+            Console.WriteLine("Banda:CAPITAL INICIAL");
+
+
+                           Console.WriteLine("Meu caminho é cada manhã");
+                           Console.WriteLine("Não procure saber onde estou");
+                           Console.WriteLine("meus destino não é de ninguem");
+                           Console.WriteLine("E eu não deixo meus passos no chão");
+                           Console.WriteLine("Se você não entende não vê");
+                           Console.WriteLine("Se não me vê não entende");
+
+                           Console.WriteLine("Não procure saber onde estou");
+                           Console.WriteLine("Se o meu jeito te surpreende");
+                         Console.WriteLine("Se o meu corpo virasse sol");
+                     Console.WriteLine("Minha mente virasse sol");
+                 Console.WriteLine("Mas só chove, chove");
+              Console.WriteLine("chove,chove");
+
+                           Console.WriteLine("Se um dia eu pudesse ver");
+                           Console.WriteLine("Meu passado inteiro");
+                           Console.WriteLine("E fizesse parar de chover");
+                           Console.WriteLine("Nos primeiro erros ah");
+            
+
+                           Console.WriteLine("Meu corpo viraria sol");
+                           Console.WriteLine("Minha mente viraria sol");
+                     Console.WriteLine("Mas só chove,chove");
+                  Console.WriteLine("chove,chove");
+        
+        
+                                 Console.WriteLine("Se um dia eu pudesse ver");
+                              Console.WriteLine("Meu passado inteiro");
+                            Console.WriteLine("E fizesse parar de chover");
+                           Console.WriteLine("Nos primeiro erros ah");
+            
+            
+                       Console.WriteLine("Meu corpo viraria sol");
+                   Console.WriteLine("Minha mente viraria sol");
+                 Console.WriteLine("Mas só chove,chove");
+            Console.WriteLine("chove,chove");
+            
+            
+                         Console.WriteLine("Meu corpo viraria sol");
+                       Console.WriteLine("Minha mente viraria");
+                   Console.WriteLine("Mas só chove,chove");
+                Console.WriteLine("chove,chove");
+            
+                   Console.WriteLine("Meu corpo viraria sol");
+                Console.WriteLine("Minha mente viraria sol");
+               Console.WriteLine("Mas só chove,chove");
+            Console.WriteLine("chove,chove");
+
+
+
+
+
+
+        }
+    }
+}
